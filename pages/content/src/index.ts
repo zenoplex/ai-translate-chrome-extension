23ddb0486b36d6ff9012a8f4fc0667c9d62f99c2
Gen Ichino <zenoplex@gmail.com>
2024-12-09 00:16:01 +0900

    feat: Implement language detection on text selection with debounce

--- pages/content/src/index.ts
+++ pages/content/src/index.ts
@@ -1,5 +1,22 @@
-import { toggleTheme } from '@src/toggleTheme';
-
+import { debounce } from './utils';
+import { create } from './languageDetector';
 console.log('content script loaded');
 
-void toggleTheme();
+const main = async () => {
+  // TODO: Error handling
+  const detector = await create();
+
+  const selectionChangeHandler = async () => {
+    const selection = document.getSelection();
+    if (!selection) return;
+
+    const results = await detector.detect(selection.toString());
+    if (results.length < 1) return;
+
+    console.log(results[0].detectedLanguage);
+  };
+
+  document.addEventListener('selectionchange', debounce(selectionChangeHandler));
+};
+
+main();
