594c3aa9f569e209923b7c00e331dcc989d1a0a6
Gen Ichino <zenoplex@gmail.com>
2024-12-10 00:28:59 +0900

    feat: Add daisyui plugin to Tailwind CSS configuration

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from 'tailwindcss/types/config';
+import daisyui from 'daisyui';
 
 export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 } as Omit<Config, 'content'>;
