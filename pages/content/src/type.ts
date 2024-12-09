8010cd2a2bf9493950d1e5e5a4aef04ddabc0cdf
Gen Ichino <zenoplex@gmail.com>
2024-12-09 23:51:06 +0900

    feat: Add LanguageTranslator interface

--- pages/content/src/type.ts
+++ pages/content/src/type.ts
@@ -6,3 +6,13 @@ export interface LanguageDetector {
    */
   ready: Promise<void>;
 }
+
+export interface LanguageTranslator {
+  translate: (str: string) => Promise<string>;
+  destroy: () => void;
+  /**
+   * According to the document it exists when canDetect returns after-download.
+   * However, I couldn't find any code that assigns a value to this property.
+   */
+  ready: Promise<void>;
+}
