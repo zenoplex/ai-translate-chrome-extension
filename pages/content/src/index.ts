0ed492b17fc5c51e7c0cc17d63e10c12fa4e0d50
Gen Ichino <zenoplex@gmail.com>
2024-12-09 23:52:50 +0900

    feat: Integrate language detection and translation in content script

--- pages/content/src/index.ts
+++ pages/content/src/index.ts
@@ -1,10 +1,10 @@
 import { debounce } from './utils';
-import { create } from './languageDetector';
-console.log('content script loaded');
+import { create as createDetector } from './languageDetector';
+import { create as createTranslator } from './languageTranslator';
 
 const main = async () => {
   // TODO: Error handling
-  const detector = await create();
+  const detector = await createDetector();
 
   const selectionChangeHandler = async () => {
     const selection = document.getSelection();
@@ -13,7 +13,16 @@ const main = async () => {
     const results = await detector.detect(selection.toString());
     if (results.length < 1) return;
 
-    console.log(results[0].detectedLanguage);
+    // TODO: target language should be configurable
+    const targetLanguage = 'ja';
+    const sourceLanguage = results[0].detectedLanguage;
+    if (sourceLanguage === targetLanguage) return;
+
+    const translator = await createTranslator({ sourceLanguage, targetLanguage });
+    const result = await translator.translate(selection.toString());
+    console.log(result);
+
+    translator.destroy();
   };
 
   document.addEventListener('selectionchange', debounce(selectionChangeHandler));
