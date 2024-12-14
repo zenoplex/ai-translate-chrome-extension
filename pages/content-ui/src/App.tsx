4faccb578aed5001691a50af734dc748759ed39f
Gen Ichino <zenoplex@gmail.com>
2024-12-14 22:24:52 +0900

    feat: Enhance Popover positioning by creating a reference element dynamically based on translated selection

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -2,22 +2,40 @@ import { PopoverContent } from '@extension/ui';
 import { useStorage } from '@extension/shared';
 import { translatedSelectionStorage } from '@extension/storage';
 import { usePopper } from 'react-popper';
-import { useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
   const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
+  const [refElement, setRefElement] = useState<HTMLDivElement | null>(null);
 
   const translatedSelection = useStorage(translatedSelectionStorage);
 
-  const reference = useMemo(() => {
-    if (!translatedSelection?.rect) return null;
-    return { getBoundingClientRect: () => DOMRect.fromRect(translatedSelection.rect) };
-  }, [translatedSelection]);
+  const { styles, attributes } = usePopper(refElement, popperElement, {
+    placement: 'top',
+    modifiers: [{ name: 'offset', options: { offset: [0, 20] } }],
+  });
 
-  const { styles, attributes } = usePopper(reference, popperElement);
+  useEffect(() => {
+    if (!translatedSelection?.rect) return;
+
+    const refElement = document.createElement('div');
+    refElement.style.position = 'absolute';
+    refElement.style.top = `${translatedSelection.rect.top + window.scrollY}px`;
+    refElement.style.left = `${translatedSelection.rect.left + window.scrollX}px`;
+    refElement.style.width = `${translatedSelection.rect.width}px`;
+    refElement.style.height = `${translatedSelection.rect.height}px`;
+    document.body.append(refElement);
+
+    setRefElement(refElement);
+
+    return () => {
+      refElement.remove();
+      setRefElement(null);
+    };
+  }, [translatedSelection.rect]);
 
   return translatedSelection.text ? (
-    <div ref={setPopperElement} style={{ ...styles.popper, backgroundColor: 'red' }} {...attributes.popper}>
+    <div ref={setPopperElement} className="max-w-xl" style={styles.popper} {...attributes.popper}>
       <PopoverContent content={translatedSelection.text} />
     </div>
   ) : null;
